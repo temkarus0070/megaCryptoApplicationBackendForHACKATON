8080b69b76631ed2835771a99f714841f6aed873
temkarus0070 <pretkovartyom@gmail.com>
2022-02-18 15:10:39 +0300

    first

---
+++ src/main/java/org/neoflex/megacryptoapplicationbackend/MegaCryptoApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.neoflex.megacryptoapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaCryptoApplicationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaCryptoApplicationBackendApplication.class, args);
+    }
+
+}
